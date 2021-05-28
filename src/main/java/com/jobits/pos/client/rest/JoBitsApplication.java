c33917dfbd07ffd859ef75df910c05169c8e3ba7
Jorge Hurtado <jjhurtado796@gmail.com>
2021-05-28 00:51:39 -0400

    Acoplado ubicaciones

--- src/main/java/com/jobits/pos/client/rest/JoBitsApplication.java
+++ src/main/java/com/jobits/pos/client/rest/JoBitsApplication.java
@@ -1,5 +1,6 @@
 package com.jobits.pos.client.rest;
 
+import com.jobits.pos.core.client.rest.CoreRESTClientConfig;
 import com.jobits.pos.reserva.client.rest.ReservaRESTClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +9,9 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {
     ReservaRESTClientConfig.BASE_PACKAGE,
-    
+    CoreRESTClientConfig.BASE_PACKAGE,
     "com.jobits.pos.client.rest"})
+    })
 public class JoBitsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jobits/pos/client/rest/ServletInitializer.java
+++ src/main/java/com/jobits/pos/client/rest/ServletInitializer.java
@@ -1,13 +1,15 @@
 package com.jobits.pos.client.rest;
 
+import org.jobits.db.core.module.DataVersionControlModule;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(JoBitsApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        DataVersionControlModule.init();
+        return application.sources(JoBitsApplication.class);
+    }
 
 }
