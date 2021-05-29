2f42de25b78a329397b87074da1c916c70dede65
Erik <erikquesada23@gmail.com>
2021-05-28 22:39:14 -0400

    DataVersionControlModule inizializado

--- src/main/java/com/jobits/pos/client/rest/JoBitsApplication.java
+++ src/main/java/com/jobits/pos/client/rest/JoBitsApplication.java
@@ -2,6 +2,7 @@ package com.jobits.pos.client.rest;
 
 import com.jobits.pos.core.client.rest.CoreRESTClientConfig;
 import com.jobits.pos.reserva.client.rest.ReservaRESTClientConfig;
+import org.jobits.db.core.module.DataVersionControlModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class JoBitsApplication {
 
     public static void main(String[] args) {
+        DataVersionControlModule.init();
         SpringApplication.run(JoBitsApplication.class, args);
     }
 
